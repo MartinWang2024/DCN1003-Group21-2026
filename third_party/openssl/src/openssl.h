c83597013e4fc634518f7e072654097be187e6b0
Rinsutoringu <Chord_sky@outlook.com>
2026-05-12 01:58:06 +0800

    format: format openssl driver

--- third_party/openssl/src/openssl.h
+++ third_party/openssl/src/openssl.h
@@ -1,4 +1,12 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <winsock2.h> // 必须在 openssl 之前
+#include "../libopenssl/include/openssl/evp.h"
+#include "../libopenssl/include/openssl/sha.h"
+#include "../libopenssl/include/openssl/err.h"
 
 std::string sha256(const std::string& str);
 /**
@@ -9,3 +17,5 @@ std::string sha256(const std::string& str);
  * @return 密文
  */
 std::vector<unsigned char> aes_encrypt(const std::string& plain_text, const unsigned char* key, const unsigned char* iv);
+
+std::vector<unsigned char> aes_decrypt(const std::string& cipher_text, const unsigned char* key, const unsigned char* iv);
