91253e10e371d0a8332e60e2ce83475b449ffe9f
Rinsutoringu <Chord_sky@outlook.com>
2026-05-15 05:20:33 +0800

    feat: add server cmd

--- programs/Server/include/cmdreg.h
+++ programs/Server/include/cmdreg.h
@@ -8,6 +8,7 @@ Response_t handle_query_code(const ReqContext_t& ctx);
 Response_t handle_query_instructor(const ReqContext_t& ctx);
 Response_t handle_query_semester(const ReqContext_t& ctx);
 Response_t handle_view_all(const ReqContext_t& ctx);
+Response_t handle_view_all_page(const ReqContext_t& ctx);
 Response_t handle_add(const ReqContext_t& ctx);
 Response_t handle_update(const ReqContext_t& ctx);
 Response_t handle_delete(const ReqContext_t& ctx);
